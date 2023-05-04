a56a0c73483492ef860a2df6867ea86b8ddb4c4d
Roman SHamagin <roman.shamagin@7vpn.com>
2023-05-04 17:41:23 +0300

    add ignore qcow2

--- start-raspios-bullseye-arm64.sh
+++ start-raspios-bullseye-arm64.sh
@@ -2,11 +2,12 @@
 
 # Set the path to the RaspiOS Bullseye arm64 image
 IMAGE="2023-05-03-raspios-bullseye-arm64.img"
+IMAGE_QCOW2="2023-05-03-raspios-bullseye-arm64.qcow2"
 IMAGE_XZ="${IMAGE}.xz"
 URL="https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-05-03/${IMAGE_XZ}"
 
 # Set the path to the QEMU ARM64 BIOS
-BIOS="/opt/homebrew/share/qemu/edk2-aarch64-code.fd"
+BIOS="/opt/homebrew/Cellar/qemu/8.0.0/share/qemu/edk2-aarch64-code.fd"
 
 # Check if the image file exists
 if [ ! -f "${IMAGE}" ]; then
@@ -25,6 +26,21 @@ if [ ! -f "${IMAGE}" ]; then
   fi
 fi
 
+# Convert the image to qcow2 format
+if [ ! -f "${IMAGE_QCOW2}" ]; then
+  echo "Converting the image to qcow2 format..."
+  qemu-img convert -f raw -O qcow2 "${IMAGE}" "${IMAGE_QCOW2}"
+fi
+
+# Check and resize the image if necessary
+REQUIRED_SIZE=8589934592 # 8 GiB in bytes
+CURRENT_SIZE=$(qemu-img info --output=json "${IMAGE_QCOW2}" | jq '.["virtual-size"]')
+
+if [ "${CURRENT_SIZE}" -lt "${REQUIRED_SIZE}" ]; then
+  echo "Resizing the image to 8 GiB..."
+  qemu-img resize "${IMAGE_QCOW2}" 8G
+fi
+
 # Check if the BIOS file exists
 if [ ! -f "${BIOS}" ]; then
   echo "BIOS file '${BIOS}' not found. Please ensure QEMU is installed correctly and the path to the BIOS file is correct."
@@ -33,17 +49,11 @@ fi
 
 # Start QEMU
 qemu-system-aarch64 \
-  -M virt \
-  -m 2048 \
-  -smp 4 \
-  -cpu cortex-a72 \
-  -bios "${BIOS}" \
-  -serial stdio \
-  -device virtio-gpu-pci \
-  -device usb-ehci \
-  -device usb-kbd \
-  -device usb-mouse \
-  -drive file="${IMAGE}",if=virtio,format=raw \
-  -netdev user,id=vmnic \
-  -device virtio-net-device,netdev=vmnic \
-  -display default
+   -machine raspi3b \
+   -cpu cortex-a72 \
+   -dtb bcm2710-rpi-3-b-plus.dtb \
+   -m 1G -smp 4 -serial stdio \
+   -kernel kernel8.img \
+   -sd ./2023-05-03-raspios-bullseye-arm64.qcow2 \
+   -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2 rootdelay=1" \
+   -device usb-mouse -device usb-kbd
